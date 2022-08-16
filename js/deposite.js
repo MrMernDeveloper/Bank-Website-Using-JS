a3a19973df22d628e426a8d8a2742e91d29249e0
MrMernDeveloper <rakibkst16@gmail.com>
2022-08-16 11:57:05 +0600

    comple deposite section

--- js/deposite.js
+++ js/deposite.js
@@ -1,13 +1,27 @@
 
 //step-1: add event handler to the deosite button
 document.getElementById('button-deposite').addEventListener('click', function () {
+ 
+   
+
     // get the deposite amount from the deposite input field
     const depositeText = document.getElementById('deposite-email');
-    const depositeAmount = depositeText.value;
+    const newDepositeAmount = parseFloat(depositeText.value);
+    console.log(typeof newDepositeAmount);
    
 
     // get the current deposite total
     const desposElement = document.getElementById('deposite-total');
-    const depositeTotal = desposElement.innerText;
-    desposElement.innerText = depositeAmount;
+    const previousDepositeTotal = parseFloat(desposElement.innerText);
+    const depositeTotal = newDepositeAmount + previousDepositeTotal
+    desposElement.innerText = depositeTotal;
+    
+    // get balance current total
+    const balancetext = document.getElementById('total-balance');
+    const balanceTotal = parseFloat(balancetext.innerText);
+    const currentBalance = balanceTotal + newDepositeAmount;
+    balancetext.innerText = currentBalance;
+
+   // clear deposite text
+    depositeText.value = ''
 })
